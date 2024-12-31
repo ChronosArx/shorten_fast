70c5520b5df6b499d6114790853a39ea385eea25
David Arias <chronos.software.dev@gmail.com>
2024-12-31 04:12:24 -0700

    configuracion para carpeta templates en raiz

--- shorten_fast/config/base.py
+++ shorten_fast/config/base.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "shorten_fast.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
