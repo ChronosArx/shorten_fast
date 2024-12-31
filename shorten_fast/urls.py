def00f943486d4f0ffadad7c71993ed44b569551
David Arias <chronos.software.dev@gmail.com>
2024-12-31 03:28:32 -0700

    corrección de url para uso de carpeta api

--- shorten_fast/urls.py
+++ shorten_fast/urls.py
@@ -1,32 +1,15 @@
-"""
-URL configuration for shorten_fast project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
-from shorten.views import Redirects
+from shorten.api.views import Redirects
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("shorten.urls")),
-    path("api/", include("authentication.urls")),
+    path("api/", include("shorten.api.urls")),
+    path("api/", include("authentication.api.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/docs/",
