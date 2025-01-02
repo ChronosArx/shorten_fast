fbeb18163868783a5b74ef25189afef24d72ceaa
David Arias <chronos.software.dev@gmail.com>
2025-01-02 02:23:23 -0700

    eliminación de mixin y uso de un solo serializer

--- shorten/serializers.py
+++ shorten/serializers.py
@@ -2,13 +2,12 @@ from rest_framework import serializers
 from .models import ShortLink
 
 
-class ShortLinkCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ShortLink
-        fields = ["title", "original_url"]
-
-
 class ShortLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShortLink
         fields = "__all__"
+        extra_kwargs = {
+            "short_url": {"read_only": True},
+            "code": {"read_only": True},
+            "user_id": {"read_only": True},
+        }
