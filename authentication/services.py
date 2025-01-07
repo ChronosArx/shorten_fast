05fd4e74f5041f90ef7c8baf3d7bd49d850b8ffa
David Arias <chronos.software.dev@gmail.com>
2025-01-07 02:23:23 -0700

    uso de parametro email

--- authentication/services.py
+++ authentication/services.py
@@ -7,8 +7,8 @@ resend.api_key = RESEND_API_KEY
 def send_confirm_email(email: str, code: str):
     params: resend.Emails.SendParams = {
         "from": "Acme <onboarding@resend.dev>",
-        "to": ["chronos.software.dev@gmail.com"],
-        "subject": "hello world",
-        "html": f"<strong>Your code is {code}!</strong>",
+        "to": [f"{email}"],
+        "subject": "Confirm Code.",
+        "html": f"<p>Your confirm code is <strong>{code}</strong> !!</p>",
     }
     resend.Emails.send(params)
