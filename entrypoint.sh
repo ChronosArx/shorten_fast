2d68e3532bea5a0767b3b5382c0abccd3eb001f6
David Arias <chronos.software.dev@gmail.com>
2025-01-14 00:02:23 -0700

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ echo 'Applying migrations...'
 python manage.py migrate
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTING_MODULE=config.settings.production shorten_fast.wsgi:application --bind 0.0.0.0:8000
+gunicorn shorten_fast.wsgi:application --bind 0.0.0.0:8000
