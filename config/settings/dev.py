25d1ace9dfecfbc2565db59c9207e9dbc38ce80f
David <david.arx.dev@gmail.com>
2026-05-25 23:26:56 -0700

    refactor: quit default in database dev

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -9,7 +9,7 @@ SECRET_KEY = env.str(
 EXPIRE_TIME_TOKEN_MINUTES = env.int("EXPIRE_TIME_TOKEN_MINUTES", default=5)
 EXPIRE_TIME_TOKEN_DAYS = env.int("EXPIRE_TIME_TOKEN_DAYS", default=7)
 CORS_ALLOW_ALL_ORIGINS = True
-DATABASES = {"default": env.db("DATABASE_URL", default="sqlite:///db.sqlite3")}
+DATABASES = {"default": env.db("DATABASE_URL")}
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=EXPIRE_TIME_TOKEN_MINUTES),
