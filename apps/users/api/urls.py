02a475b8ba6ca80e3b313ad4d4c612829d538f76
David <david.arx.dev@gmail.com>
2026-08-13 01:07:24 -0600

    refactor: update API endpoint names for consistency in user authentication tests

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from .views import Register, CustomTokenObtainPairView, CustomTokenRefreshView
 
+app_name = "api"
+
 urlpatterns = [
-    path("auth/register/", Register.as_view(), name="api_register"),
-    path(
-        "auth/login/", CustomTokenObtainPairView.as_view(), name="api_token_obtain_pair"
-    ),
-    path("auth/refresh/", CustomTokenRefreshView.as_view(), name="api_token_refresh"),
+    path("auth/register/", Register.as_view(), name="user_register"),
+    path("auth/login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
 ]
